9f267ab9f7aebe78c4ee14e03fe17dff0c7b150b
Drew Dara-Abrams <drew@mapzen.com>
2015-10-13 07:45:49 -0700

    store Datastore API auth token in browser's localStorage

--- app/components/auth-token-input/component.js
+++ app/components/auth-token-input/component.js
@@ -1,6 +1,16 @@
 import Ember from 'ember';
 
+var LOCALSTORAGE_KEY = 'transitland-dispatcher-auth-token';
+
 export default Ember.Component.extend({
   session: Ember.inject.service(),
-  authToken: Ember.computed.alias('session.authToken')
+  authToken: Ember.computed.alias('session.authToken'),
+  readStoredAuthToken: function() {
+    this.set('authToken', localStorage.getItem(LOCALSTORAGE_KEY));
+  }.on('init'),
+  actions: {
+    change: function(value) {
+      localStorage.setItem(LOCALSTORAGE_KEY, value);
+    }
+  }
 });
