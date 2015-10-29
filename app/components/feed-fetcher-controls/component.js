13f4311183f859e155dab3c527be3b3c09f53cea
Drew Dara-Abrams <drew@mapzen.com>
2015-10-29 15:17:01 -0700

    fix `feed-fetcher-controls` component
    Components don't have access to the store, unless it's injected.

--- app/components/feed-fetcher-controls/component.js
+++ app/components/feed-fetcher-controls/component.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
   actions: {
     fetchFeeds: function() {
       var adapter = this.get('store').adapterFor('feed');
