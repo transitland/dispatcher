7543c65b03f1021f065651603c9a99c8e917fb64
Drew Dara-Abrams <drew@mapzen.com>
2016-01-29 17:30:46 -0800

    to delete a `ChangePayload`, you now have to open it for editing

--- app/changesets/change-payloads/edit/route.js
+++ app/changesets/change-payloads/edit/route.js
@@ -5,9 +5,21 @@ export default Ember.Route.extend({
     return this.store.find('change-payload', params['change_payload_id']);
   },
   actions: {
+    delete: function() {
+      var self = this;
+      var model = self.currentModel;
+      var changeset = model.get('changeset');
+      const flashMessages = Ember.get(this, 'flashMessages');
+      model.destroyRecord().then(() => {
+        flashMessages.success("Change payload deleted!");
+        self.transitionTo('changesets.show', changeset);
+      }).catch(function(error) {
+        flashMessages.danger(`Error(s) deleting change payload: ${error.message}`);
+      });
+    },
     update: function() {
       var self = this;
-      var model = this.currentModel;
+      var model = self.currentModel;
       var changeset = model.get('changeset');
       const flashMessages = Ember.get(this, 'flashMessages');
       model.set('payload', JSON.parse(model.get('stringified_payload')));
