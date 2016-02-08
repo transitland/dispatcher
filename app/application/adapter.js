8da6a7ac94a0f52432d1d63fa33d881240229ee4
Drew Dara-Abrams <drew@mapzen.com>
2016-02-08 07:10:06 -0800

    returns an error when user tries to enqueue a feed import
    fixes #19

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -16,9 +16,16 @@ export default DS.RESTAdapter.extend({
   }),
   ajaxOptions: function(url, type, options) {
     var hash = this._super(url, type, options);
-    if (typeof(ENV.apiProxyKey) !== "undefined") {
-      hash.data = hash.data || {};
-      hash.data["api_key"] = ENV.apiProxyKey;
+    // only need to include api_key when making GET requests
+    // because those are the most frequent type of request.
+    // if we include api_key in POSTs or PUTs, Datastore will barf
+    if (typeof(ENV.apiProxyKey) !== "undefined" && type === 'GET') {
+      let data = {};
+      if (typeof(hash.data) === 'string') {
+        data = JSON.parse(hash.data);
+      }
+      data["api_key"] = ENV.apiProxyKey;
+      hash.data = data;
     }
     return hash;
   }
