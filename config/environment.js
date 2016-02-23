fa883e36c6c92ea4042ad33dc39584befd159296
Drew Dara-Abrams <drew@mapzen.com>
2016-02-23 15:42:33 -0800

    misc. environment improvements

--- config/environment.js
+++ config/environment.js
@@ -17,7 +17,7 @@ module.exports = function(environment) {
 
     contentSecurityPolicy: {
       'default-src': "'none' 'self' https://*.cloudfront.net",
-      'connect-src': "'self' https://*.transit.land http://localhost:*",
+      'connect-src': "'self' https://*.transit.land https://transit.land http://localhost:*",
       'img-src': "'self' data: ",
       "child-src": "'self' blob:",
       'style-src': "'self' 'unsafe-inline'",
@@ -65,13 +65,13 @@ module.exports = function(environment) {
   if (environment === 'staging') {
     ENV.datastoreHost = 'https://dev.transit.land';
     ENV.baseURL = '/dispatcher';
-    ENV.apiProxyKey= 'transitland-YFO6jk8';
+    ENV.apiProxyKey = 'transitland-YFO6jk8';
   }
 
   if (environment === 'production') {
     ENV.datastoreHost = 'https://transit.land';
     ENV.baseURL = '/dispatcher';
-    ENV.apiProxyKey= 'transitland-k3a9ArI';
+    ENV.apiProxyKey = 'transitland-k3a9ArI';
   }
 
   return ENV;
