8df268250e4cec64d3b6b70f14f755c72c53be76
Drew Dara-Abrams <drew@mapzen.com>
2016-03-03 21:55:21 -0500

    standardize the way we get auth token

--- app/session/service.js
+++ app/session/service.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
+import ENV from 'dispatcher/config/environment';
 
 export default Ember.Service.extend({
-  authToken: null
+  authToken: null,
+  getAuthToken: function() {
+    if (this.get('authToken') !== null) {
+      return this.get('authToken');
+    } else {
+      return localStorage.getItem(ENV.AUTH_TOKEN_LOCALSTORAGE_KEY);
+    }
+  }
 });
