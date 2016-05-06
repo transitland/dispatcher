8dca4bc3bb9f9ac086419143f0204b7bc196f0ae
Ian Rees <ian@ianrees.net>
2016-05-06 16:49:11 -0700

    Add active_feed_version_update to Feed query params and controller

--- app/feeds/index/controller.js
+++ app/feeds/index/controller.js
@@ -2,4 +2,5 @@ import Ember from 'ember';
 import PaginatedController from 'dispatcher/mixins/paginated-controller';
 
 export default Ember.Controller.extend(PaginatedController, {
+  active_feed_version_update: false
 });
--- app/feeds/index/route.js
+++ app/feeds/index/route.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 import PaginatedRoute from 'dispatcher/mixins/paginated-route';
 
 export default Ember.Route.extend(PaginatedRoute, {
+  queryParams: {
+    active_feed_version_update: {
+      refreshModel: true
+    }
+  },
   model: function(params) {
     return this.store.query('feed', params);
   }
