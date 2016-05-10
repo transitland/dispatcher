66f58bcc96f01c4b88a827c8662ba3275923835b
Ian Rees <ian@ianrees.net>
2016-05-10 16:15:31 -0700

    Feed expiration actions

--- app/feeds/index/controller.js
+++ app/feeds/index/controller.js
@@ -3,6 +3,18 @@ import PaginatedController from 'dispatcher/mixins/paginated-controller';
 
 export default Ember.Controller.extend(PaginatedController, {
   activeFeedVersionUpdate: false,
-  activeFeedVersionExpired: '2016-05-10',
-  activeFeedVersionValid: '2016-05-10'
+  activeFeedVersionExpired: null,
+  activeFeedVersionValid: null,
+  actions: {
+    setActiveFeedVersionExpired(date) {
+      // Convert to YYYY-MM-DD
+      date = date ? date.toISOString().slice(0,10) : "";
+      this.set('activeFeedVersionExpired', date);
+    },
+    setActiveFeedVersionValid(date) {
+      // Convert to YYYY-MM-DD
+      date = date ? date.toISOString().slice(0,10) : "";
+      this.set('activeFeedVersionValid', date);
+    }
+  }
 });
