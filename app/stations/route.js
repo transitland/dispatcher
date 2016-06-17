d0393f4b4d3cc2eee0e459c43f2e3a5891ecb6b7
Ian Rees <ian@ianrees.net>
2016-06-16 22:55:26 -0700

    Remove debug

--- app/stations/route.js
+++ app/stations/route.js
@@ -3,7 +3,6 @@ import PaginatedSortableRoute from 'dispatcher/mixins/paginated-sortable-route';
 
 export default Ember.Route.extend(PaginatedSortableRoute, {
   model: function(params) {
-    console.log('stations');
     return this.store.query('stop-station', params);
   }
 });
