ae679027acaba73dc13f0f7661e441dcbf774ed3
Ian Rees <ian@ianrees.net>
2016-06-16 18:17:32 -0700

    StopStation relations

--- app/stop-station/model.js
+++ app/stop-station/model.js
@@ -4,7 +4,22 @@ import EntityWithActivityModel from 'dispatcher/entity-with-activity/model';
 
 export default EntityWithActivityModel.extend({
 	created_or_updated_in_changeset: DS.belongsTo('changeset', { async: true }),
+	onestop_id: Ember.computed.alias('id'),
+	name: DS.attr('string'),
 	created_at: DS.attr('date'),
 	updated_at: DS.attr('date'),
-	onestop_id: Ember.computed.alias('id')
+	geometry: DS.attr(),
+	tags: DS.attr(),
+  parent_stop: DS.belongsTo('stop-station', { async: true, modelFor: 'stop-station' }),
+  stop_platforms: DS.hasMany('stop-station', { async: true, modelFor: 'stop-station', inverse: 'parent_stop'}),
+  // stop_egresses: DS.hasMany('stop-station', { async: true, modelFor: 'stop-station' }),
+  lat: Ember.computed('geometry', function() {
+    return this.get('geometry').coordinates[1]
+  }),
+  lng: Ember.computed('geometry', function() {
+    return this.get('geometry').coordinates[0]
+  }),
+  stationArea: Ember.computed('stop_platforms', 'stop_egresses', function() {
+    return this.get('stop_platforms').map(function(sp){return sp.get('geometry').coordinates.reverse()});
+  }),
 });
--- app/stop-station/serializer.js
+++ app/stop-station/serializer.js
@@ -1,7 +1,10 @@
 import ApplicationSerializer from '../application/serializer';
 
-export default ApplicationSerializer.extend({
+export default ApplicationSerializer.extend(DS.EmbeddedRecordsMixin, {
   primaryKey: 'onestop_id',
   attrs: {
+    stop_platforms: {
+      deserialize: 'records'
+    }
   }
 });
