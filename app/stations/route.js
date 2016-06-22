a18280b5ce30997d2cd77d6cb7f15ba902cd0d69
Ian Rees <ian@ianrees.net>
2016-06-22 14:52:58 -0700

    Use 'replace'

--- app/stations/route.js
+++ app/stations/route.js
@@ -4,6 +4,7 @@ import PaginatedSortableRoute from 'dispatcher/mixins/paginated-sortable-route';
 export default Ember.Route.extend(PaginatedSortableRoute, {
   queryParams: {
     bbox: {
+      replace: true,
       refreshModel: true
     }
   },
