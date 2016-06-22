d305ec3f3a5e289990b4964af5fe6a864ed1e6c2
Drew Dara-Abrams <drew@mapzen.com>
2016-06-21 18:31:43 -0700

    user select should close when an option is selected
    related to #66

--- app/components/user-select/component.js
+++ app/components/user-select/component.js
@@ -3,10 +3,5 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   userMatcher(user, term) {
     return (user.get('name') + ' ' + user.get('email')).indexOf(term);
-  },
-  actions: {
-    handleFocus(select) {
-      select.actions.open();
-    }
   }
 });
