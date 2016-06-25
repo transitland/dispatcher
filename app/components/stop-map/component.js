d7f1a8f8f569a7432f6b632bb15ecb1455630439
Ian Rees <ian@ianrees.net>
2016-06-24 18:24:02 -0700

    Component-ify

--- app/components/stop-map/component.js
+++ app/components/stop-map/component.js
@@ -1,4 +1,35 @@
 import Ember from 'ember';
+/* global L */
+
+var stopPlatformIcon = L.icon({
+    iconUrl: 'assets/images/marker-icon.png',
+    iconRetinaUrl: 'assets/images/marker-icon-2x.png',
+    iconSize: [38, 95],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+    shadowUrl: 'my-icon-shadow.png',
+    shadowRetinaUrl: 'my-icon-shadow@2x.png',
+    shadowSize: [68, 95],
+    shadowAnchor: [22, 94]
+});
 
 export default Ember.Component.extend({
+  stopIconTest: stopPlatformIcon,
+  actions: {
+    updateView(e) {
+      let center = e.target.getCenter();
+      var zoom = e.target.getZoom();
+      var bounds = e.target.getBounds();
+      this.get('onView')(center.lat, center.lng, zoom);
+      this.get('onBounds')(bounds);
+    },
+    updateLocation(r, e) {
+      console.log('updateLocation:', r.id);
+      let location = e.target.getLatLng();
+      Ember.setProperties(r, {
+        lat: location.lat,
+        lng: location.lng
+      });
+    }
+  }
 });
--- app/stations/controller.js
+++ app/stations/controller.js
@@ -2,39 +2,21 @@ import Ember from 'ember';
 import PaginatedSortableController from 'dispatcher/mixins/paginated-sortable-controller';
 /* global L */
 
-var stopPlatformIcon = L.icon({
-    iconUrl: 'assets/images/marker-icon.png',
-    iconRetinaUrl: 'assets/images/marker-icon-2x.png',
-    iconSize: [38, 95],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-    shadowUrl: 'my-icon-shadow.png',
-    shadowRetinaUrl: 'my-icon-shadow@2x.png',
-    shadowSize: [68, 95],
-    shadowAnchor: [22, 94]
-});
-
 export default Ember.Controller.extend(PaginatedSortableController, {
-  lat: 37.32955,
-  lng: -121.90311,
+  lat: 37.78990,
+  lng: -122.39310,
   zoom: 14,
-  bbox: null,
   per_page: 10,
-  stopIconTest: stopPlatformIcon,
   actions: {
     saveStops() {
     },
-    updateView(e) {
-      var bounds = e.target.getBounds();
-      console.log("Updated view: ", bounds);
+    updateBounds(bounds) {
       this.set('bbox', bounds.toBBoxString());
     },
-    updateLocation(r, e) {
-      let location = e.target.getLatLng();
-      Ember.setProperties(r, {
-        lat: location.lat,
-        lng: location.lng
-      });
+    updateView(lat, lng, zoom) {
+      this.set('lat', lat);
+      this.set('lng', lng);
+      this.set('zoom', zoom);
     }
   }
 });
