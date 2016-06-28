dedce6b99144d90bb643f1d898a385be05488c41
Ian Rees <ian@ianrees.net>
2016-06-28 15:30:06 -0700

    Valhalla lookahead

---
+++ app/valhalla-lookahead/controller.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  departure_date_times: [0,7].map(function(offset) {
+    var today = new Date();
+    var tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate()+offset);
+    return tomorrow;
+  }),
+  sample_routes: 1
+});
---
+++ app/valhalla-lookahead/route.js
@@ -0,0 +1,91 @@
+import Ember from 'ember';
+
+// Fisher-Yates Shuffle from Mike Bostock
+// https://bost.ocks.org/mike/shuffle/
+function shuffle_sample(array, count) {
+  var m = array.length, t, i;
+  // While there remain elements to shuffle…
+  while (m) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array.slice(0,count);
+}
+
+export default Ember.Route.extend({
+  model: function(params) {
+    var self = this;
+
+    // Find feeds
+    return this.store.query('feed', {
+      per_page: 2,
+      tag_key: 'demo_feed',
+      tag_value: 'true'
+    }).then(function(feeds) {
+
+      // Find total routes for feed
+      return Ember.RSVP.all(feeds.map(function(feed){
+        return self.store.query('route', {
+          imported_from_feed: feed.id,
+          per_page: 0,
+          total: true
+        }).then(function(result) {
+
+          // Sample routes for each feed
+          var route_sample = [];
+          for (var i=0; i < result.meta.total; i++) { route_sample.push(i) }
+          route_sample = shuffle_sample(route_sample, 2);
+
+          // Find routes
+          return Ember.RSVP.all(route_sample.map(function(offset) {
+            return self.store.query('route', {
+              imported_from_feed: feed.id,
+              per_page: 1,
+              offset: offset,
+              total: false
+            }).then(function(result) {
+
+              // Sample stops for each route
+              var route = result.get('firstObject');
+              return shuffle_sample(route.get('stops_served_by_route'), 2).map(function(i) { return i.stop_onestop_id })
+
+            });
+          })).then(function(results) {
+
+            // Return the feed and stop pairs
+            return {
+              feed: feed,
+              stop_pairs: results
+            }
+
+          });
+        });
+      }))
+    }).then(function(results) {
+
+      // Flatten all stop_pair onestop_ids, fetch stops, map to stop records
+      var stop_onestop_ids = [];
+      results.forEach(function(i){
+        i.stop_pairs.forEach(function(j){
+          stop_onestop_ids.push(j[0]);
+          stop_onestop_ids.push(j[1]);
+        })
+      });
+      return self.store.query('stop', {
+        onestop_id: stop_onestop_ids
+      }).then(function(stops) {
+        var stop_hash = {};
+        stops.forEach(function(stop) { stop_hash[stop.id] = stop});
+        results.forEach(function(i) {
+          i.stop_pairs = i.stop_pairs.map(function(stop_pair) { return [stop_hash[stop_pair[0]], stop_hash[stop_pair[1]]] })
+        });
+        return results
+      })
+
+    })
+  }
+});
