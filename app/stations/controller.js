173f2f206202e166c66e6108c9d82da75f31994d
Ian Rees <ian@ianrees.net>
2016-06-30 18:02:59 -0700

    Simplify Station controller (next: remove)

--- app/stations/controller.js
+++ app/stations/controller.js
@@ -1,35 +1,8 @@
 import Ember from 'ember';
-import PaginatedSortableController from 'dispatcher/mixins/paginated-sortable-controller';
-/* global L */
 
-export default Ember.Controller.extend(PaginatedSortableController, {
+/* The only purpose of this controller is to set default query parameters */
+export default Ember.Controller.extend({
   lat: 37.78990,
   lng: -122.39310,
   zoom: 14,
-  per_page: 10,
-  actions: {
-    saveStops() {
-      var changes = this
-        .store
-        .peekAll('stop-station')
-        .filter(function(stop) {
-          return stop.get('hasDirtyAttributes')
-        })
-        .map(function(stop) {
-          return stop.toChange();
-        });
-      var changeset = {
-        changes: changes.map(function(change){return {action: 'createUpdate', change: change}})
-      }
-      console.log(JSON.stringify(changeset));
-    },
-    updateBounds(bounds) {
-      this.set('bbox', bounds.toBBoxString());
-    },
-    updateView(lat, lng, zoom) {
-      this.set('lat', lat);
-      this.set('lng', lng);
-      this.set('zoom', zoom);
-    }
-  }
 });
