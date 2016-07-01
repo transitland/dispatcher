8269ae795ea3b021cd5ecd595b1b86fdc0fe3a45
Ian Rees <ian@ianrees.net>
2016-06-30 18:03:27 -0700

    stop-map / stations controller convergence

--- app/components/stop-map/component.js
+++ app/components/stop-map/component.js
@@ -15,15 +15,41 @@ var stopPlatformIcon = L.icon({
 
 export default Ember.Component.extend({
   stopIconTest: stopPlatformIcon,
+  lat: 0,
+  lng: 0,
+  zoom: 0,
+  bounds: null,
   actions: {
+    saveStops() {
+      console.log('stop-map saveStops');
+      var changes = this.stops
+        .filter(function(stop) {
+          return stop.get('hasDirtyAttributes')
+        })
+        .map(function(stop) {
+          return stop.toChange();
+        });
+      var changeset = {
+        changes: changes.map(function(change){return {action: 'createUpdate', change: change}})
+      }
+      console.log(JSON.stringify(changeset));
+    },
+    setBounds() {
+      console.log('stop-map setBounds');
+      this.set('bbox', this.get('bounds').toBBoxString());
+    },
     updateView(e) {
+      console.log('stop-map updateView');
       let center = e.target.getCenter();
       var zoom = e.target.getZoom();
       var bounds = e.target.getBounds();
-      this.get('onView')(center.lat, center.lng, zoom);
-      this.get('onBounds')(bounds);
+      this.set('lat', center.lat);
+      this.set('lng', center.lng);
+      this.set('zoom', zoom);
+      this.set('bounds', bounds);
     },
-    updateLocation(stop, e) {
+    updateStopLocation(stop, e) {
+      console.log('stop-map updateStopLocation');
       let location = e.target.getLatLng();
       stop.setCoordinates([location.lng, location.lat]);
     }
