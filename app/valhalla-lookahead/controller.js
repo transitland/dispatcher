6aeee58b689db05487b49fbfc14db31cb0e39bba
Ian Rees <ian@ianrees.net>
2016-07-12 02:38:28 -0700

    Set default small per_page

--- app/valhalla-lookahead/controller.js
+++ app/valhalla-lookahead/controller.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  per_page: 10,
   departure_date_times: [0,7,14].map(function(offset) {
     var today = new Date();
     var tomorrow = new Date(today);
