572648c1cb0b814ea20d06108fc5c3ab8dcf96f0
Ian Rees <ian@ianrees.net>
2016-07-14 14:37:20 -0700

    Fix pagination

--- app/routecheck/controller.js
+++ app/routecheck/controller.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import PaginatedSortableController from 'dispatcher/mixins/paginated-sortable-controller';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(PaginatedSortableController, {
   per_page: 10,
   departure_date_times: [0,7,14].map(function(offset) {
     var today = new Date();
