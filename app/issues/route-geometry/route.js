ee38d3d7594314941bc40b5fa74555eced4c1063
Alexander Smith <alex@mapzen.com>
2016-07-13 17:00:55 -0700

    filtering by geometry issue type

--- app/issues/route-geometry/route.js
+++ app/issues/route-geometry/route.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model: function(params) {
+    params['issue_type'] = ['stop_rsp_distance_gap',
+                            'distance_calculation_inaccurate',
+                            'rsp_line_inaccurate',
+                            'stop_position_inaccurate'].join(',')
     var issues = this.store.query('issue', params);
     return Ember.RSVP.hash({
       issues: issues,
