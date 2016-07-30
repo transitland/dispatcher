bb11215a6b51aa36c69727e5502868ebea8733c4
Alexander Smith <alex@mapzen.com>
2016-07-29 17:27:41 -0700

    cleanup

--- app/components/modal-message/component.js
+++ app/components/modal-message/component.js
@@ -1,11 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    // close: function() {
-    //   this.$('.modal').modal('hide');
-    // }
-  },
   show: function() {
     this.$('.modal').modal().on('hidden.bs.modal', function() {
       this.sendAction('close');
