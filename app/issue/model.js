c94fe943fa982c13ec222a451bbf0ed5ccfc2975
Alexander Smith <alex@mapzen.com>
2016-09-01 11:12:18 -0700

    first attempt at polling changesets - needs work

--- app/issue/model.js
+++ app/issue/model.js
@@ -1,10 +1,9 @@
 import DS from 'ember-data';
-// import attr from 'ember-data/attr';
-// import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default DS.Model.extend({
   issue_type: DS.attr('string'),
   details: DS.attr('string'),
   open: DS.attr('boolean'),
+  //entities_with_issues: DS.hasMany('entity-with-issues', { async: true, inverse: 'issue' })
   entities_with_issues: DS.attr()
 });
--- app/issues/route-geometry/show/controller.js
+++ app/issues/route-geometry/show/controller.js
@@ -39,6 +39,23 @@ export default Ember.Controller.extend({
     });
   },
 
+  pollChangesetApply: function(url, applicationAdapter) {
+    var self = this;
+    applicationAdapter.ajax(url, 'post').then(function(response){
+      if (response.status === 'complete') {
+        self.set('applyMessage', {show: true, status: response.status, newIssues: [], message: 'Successfully resolved issue ' + self.get('model.selectedIssue.id') });
+      }
+      else if (response.status === 'error') {
+        self.set('applyMessage', {show: true, status: response.status, message: 'Error resolving issue ' + self.get('model.selectedIssue.id') + '. ' + response.errors});
+      }
+      else {
+        Ember.run.later(self.pollChangesetApply.bind(self, url, applicationAdapter), 5000);
+      }
+    }).catch(function(error){
+      self.set('applyMessage', {show: true, status: 'error', message: 'Error resolving issue ' + self.get('model.selectedIssue.id') + '. ' + error.errors.map(function(e){ return e.message}).join('. ')});
+    });
+  },
+
   actions: {
     issueClicked: function(issue) {
       if (this.get('model.selectedIssue')) {
@@ -87,26 +104,24 @@ export default Ember.Controller.extend({
           self.set('applyingSpinner', true);
           return changeset.apply_async();
         }).then(function(response) {
-          console.log(response);
           self.set('applyingSpinner', false);
           self.set('showChangeset', false);
-          if (response.status === 'queued') {
-            self.set('applyMessage', {show: true, error: false, newIssues: [], message: 'Applying changeset to resolve issue ' + self.get('model.selectedIssue.id') });
-          }
-          else if (response.status === 'error') {
-            self.set('applyingSpinner', false);
-            self.set('showChangeset', false);
-            self.set('applyMessage', {show: true, error: true, message: 'Error resolving issue ' + self.get('model.selectedIssue.id') + '. ' + error.message});
-          }
+          self.set('applyMessage', {show: true, status: response.status, newIssues: [], message: 'Applying changeset to resolve issue ' + self.get('model.selectedIssue.id') });
         }).catch(function(error) {
 
         });
     },
     toggleApplyMessage: function() {
       this.set('applyMessage.show', false);
-      if (!this.get('applyMessage').error) {
+      if (this.get('applyMessage').status === 'complete') {
         let queryParams = this.queryParamsObject();
         this.transitionToRoute('issues.route-geometry.index', { queryParams: queryParams });
+      }
+      if (this.get('applyMessage').status === 'queued') {
+        var applicationAdapter = this.store.adapterFor('changeset');
+        var modelUrl = applicationAdapter.buildURL('changeset', this.get('model.changeset.id'));
+        var applyUrl = modelUrl + '/apply_async';
+        this.pollChangesetApply(applyUrl, applicationAdapter);
       }
     },
     closeDialog: function() {
