f8bb9234b5ce27549d68c3c712b8704ccac681c3
Alexander Smith <alex@mapzen.com>
2016-09-08 14:59:23 -0700

    removing inflectors

--- app/issue/model.js
+++ app/issue/model.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 
-var inflector = Ember.Inflector.inflector;
-inflector.plural(/entity-with-issue/i, 'entities-with-issues');
-inflector.singular(/entities-with-issue/i, 'entity-with-issues');
+// var inflector = Ember.Inflector.inflector;
+// inflector.plural(/entity-with-issue/i, 'entities-with-issues');
+// inflector.singular(/entities-with-issue/i, 'entity-with-issues');
 
 export default DS.Model.extend({
   issue_type: DS.attr('string'),
   details: DS.attr('string'),
   open: DS.attr('boolean'),
-  entities_with_issues: DS.hasMany('entity-with-issues', { async: true, inverse: 'issue' })
+  entities_with_issues: DS.hasMany('entity-with-issue', { async: true, inverse: 'issue' })
 });
