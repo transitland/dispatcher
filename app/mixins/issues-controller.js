f7c5ce9d8772772a99b2706f9f64bca6acdbe536
Alexander Smith <alex@mapzen.com>
2016-11-08 12:23:45 -0800

    putting common issue code into mixins

---
+++ app/mixins/issues-controller.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Mixin.create({
+  queryParams: ['feed_onestop_id', 'open', 'issue_type', 'per_page'],
+
+  issue_type: '',
+
+  feed_onestop_id: '',
+
+  open: true,
+
+  per_page: '∞',
+
+  queryParamsObject: function() {
+    var queryParams = {};
+    var self = this;
+    this.get('queryParams').forEach(function(param) { queryParams[param] = self.get(param);  });
+    return queryParams;
+  },
+
+  actions: {
+    typeChanged: function(selected) {
+      this.set('issue_type', selected);
+      let queryParams = this.queryParamsObject();
+    }
+  }
+});
