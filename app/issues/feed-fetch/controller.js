5102be713a5c7b75fbbbed1ecbb738ea18da3e76
Alexander Smith <alex@mapzen.com>
2016-11-10 14:35:58 -0800

    removing files

--- app/issues/feed-fetch/controller.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-import IssuesController from 'dispatcher/mixins/issues-controller';
-
-export default Ember.Controller.extend(IssuesController, {
-
-});
--- app/issues/feed-fetch/route.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-import IssuesRoute from 'dispatcher/mixins/issues-route';
-
-export default Ember.Route.extend(IssuesRoute, {
-  issueTypes: ['all', 'feed_fetch_invalid_url',
-              'feed_fetch_invalid_zip',
-              'feed_fetch_invalid_response',
-              'feed_fetch_invalid_source']
-});
