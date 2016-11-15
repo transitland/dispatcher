d53a2009cbfb3ce28da52b60aa1251ee3b6ce6f7
Alexander Smith <alex@mapzen.com>
2016-11-15 13:16:31 -0800

    removing issues/feed-fetch

--- app/issues/feed-fetch/show/controller.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  getChanges: function() {
-    //return
-  },
-});
--- app/router.js
+++ app/router.js
@@ -31,9 +31,6 @@ Router.map(function() {
     });
     this.route('new', { path: 'new/:feed_id' });
     this.route('show', { path: '/:issue_id' });
-    this.route('feed-fetch', function() {
-      this.route('show', { path: '/:issue_id'});
-    });
   });
   this.route('stations');
   this.route('routingcheck');
