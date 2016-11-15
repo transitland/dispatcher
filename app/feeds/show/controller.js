cc869a91828e172afe7ce91babd41a244c3f3833
Alexander Smith <alex@mapzen.com>
2016-11-15 15:34:52 -0800

    issue details toggle

--- app/feeds/show/controller.js
+++ app/feeds/show/controller.js
@@ -1,11 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  selected: false,
+
   getChanges: function() {
     var ret = {};
     ret['action'] = 'createUpdate';
     ret['issuesResolved'] = [1];
     ret['feed'] = { url: '' };
     return [ret];
+  },
+
+  actions: {
+    issueClicked: function() {
+      this.set('selected', !this.get('selected'));
+    }
   }
 });
