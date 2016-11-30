2eb3e192aa930ac95cbae609a7ddeba7f112aed4
Alexander Smith <alex@mapzen.com>
2016-11-30 11:27:19 -0800

    edit-buttons within issue

--- app/components/issue-table/component.js
+++ app/components/issue-table/component.js
@@ -4,6 +4,12 @@ export default Ember.Component.extend({
   actions: {
     issueClicked: function(issue) {
       this.sendAction('issueClicked', issue);
+    },
+    showChangeset: function() {
+      this.sendAction('showChangeset');
+    },
+    closeDialog: function() {
+      this.sendAction('closeDialog');
     }
   }
 });
