d4d1a63049e9ed67e780db188de3d53d63fcec68
Alexander Smith <alex@mapzen.com>
2016-12-01 13:51:44 -0800

    updating messages

--- app/issue/model.js
+++ app/issue/model.js
@@ -9,11 +9,11 @@ export default DS.Model.extend({
   typeDescription: Ember.computed('issue_type', function(){
     switch (this.get('issue_type')) {
       case 'feed_fetch_invalid_url':
-        return 'The host as specified may not exist.';
+        return 'The host as specified may not exist or there may have been a network issue during fetch.';
       case 'feed_fetch_invalid_source':
         return 'GTFS feed does not contain the required files.';
       case 'feed_fetch_invalid_response':
-        return 'The host responsed, but the GTFS feed could not be retrieved.';
+        return 'The host responded, but the GTFS feed could not be found or retrieved.';
       case 'feed_fetch_invalid_zip':
         return 'Feed zip structure is not supported.';
     }
