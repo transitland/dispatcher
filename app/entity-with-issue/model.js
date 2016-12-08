5b07aa8fd762fd4dc326d16caaeaebd2afa6bba7
Alexander Smith <alex@mapzen.com>
2016-12-08 09:56:41 -0800

    entity computed prop; showing attr value in issue contents

--- app/entity-with-issue/model.js
+++ app/entity-with-issue/model.js
@@ -4,5 +4,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   issue: DS.belongsTo('issue', {inverse: 'entities_with_issues'}),
   onestop_id: DS.attr('string'),
-  entity_attribute: DS.attr('string')
+  entity_type: DS.attr('string'),
+  entity_attribute: DS.attr('string'),
+  entity: Ember.computed('onestop_id', 'entity_type', function(){
+    return this.store.findRecord(this.get('entity_type'), this.get('onestop_id'));
+  })
 });
