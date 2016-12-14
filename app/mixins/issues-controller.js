c0c99c188c7fd4412e66a9129c1ec098234ad101
Alexander Smith <alex@mapzen.com>
2016-12-14 10:33:58 -0800

    columns for issues of feed entities; loading categories; of_feed_entities_endpoint

--- app/mixins/issues-controller.js
+++ app/mixins/issues-controller.js
@@ -1,11 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-  queryParams: ['feed_onestop_id', 'open', 'issue_type', 'per_page', 'of_entity'],
+  queryParams: ['of_feed_entities', 'open', 'issue_type', 'per_page', 'of_entity'],
 
   issue_type: '',
 
-  feed_onestop_id: '',
+  of_feed_entities: '',
 
   open: true,
 
