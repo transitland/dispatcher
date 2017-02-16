4c925ebc0608d6aea67e37084521bf3d2d70fbcc
Alexander Smith <alex@mapzen.com>
2017-02-15 16:28:25 -0800

    better styling

--- app/mixins/issues-controller.js
+++ app/mixins/issues-controller.js
@@ -2,28 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   queryParams: ['of_feed_entities', 'open', 'issue_type', 'per_page', 'of_entity', 'sort_key', 'sort_order'],
-
   issue_type: '',
-
   of_feed_entities: '',
-
   open: true,
-
   per_page: '50',
-
   of_entity: '',
-
   sort_key: 'created_at',
-
   sort_order: 'desc',
-
   queryParamsObject: function() {
     var queryParams = {};
     var self = this;
     this.get('queryParams').forEach(function(param) { queryParams[param] = self.get(param);  });
     return queryParams;
   },
-
   actions: {
     issueClicked: function(issue) {
       let queryParamsObject = this.queryParamsObject();
