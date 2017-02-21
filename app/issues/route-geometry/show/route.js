85b37a930d653a83da87c83f23596177b50b9519
Alexander Smith <alex@mapzen.com>
2017-02-21 11:33:52 -0800

    WIP trying to load bbox for rsp and stop points

--- app/issues/route-geometry/show/route.js
+++ app/issues/route-geometry/show/route.js
@@ -20,38 +20,43 @@ export default Ember.Route.extend(IssuesRoute, {
       });
       changeset.get('change_payloads').createRecord();
       let users = self.store.query('user', { per_page: false });
-      var rsps = [];
-      var stops = [];
+      var rspIds = [];
+      var stopIds = [];
+      // TODO use polymorphic association on entity-with-issue for entity
       selectedIssue.get('entities_with_issues').forEach(function(entity){
         if (entity.get('onestop_id').split('-')[0] === 'r') {
-          rsps.push(entity.get('onestop_id'));
+          rspIds.push(entity.get('onestop_id'));
         }
         else if (entity.get('onestop_id').split('-')[0] === 's') {
-          stops.push(entity.get('onestop_id'));
+          stopIds.push(entity.get('onestop_id'));
         }
       });
-      return self.store.query('stop', {onestop_id: stops.join(',')}).then(function(stops){
-
-        var bounds = new L.latLngBounds(stops.map(function(stop) {
-          return new L.latLng(stop.get('coordinates'));
-        }));
-        return self.store.query('route-stop-pattern', {onestop_id: rsps.join(',')}).then(function(rsps){
-
-          rsps.forEach(function(rsp){
-            rsp.get('coordinates').forEach(function(coord){
-              bounds.extend(new L.latLng(coord));
-            });
-          });
-
-          return Ember.RSVP.hash({
-            selectedIssue: selectedIssue,
-            issueRouteStopPatterns: rsps,
-            issueStops: stops,
-            bounds: bounds,
-            changeset: changeset,
-            users: users
+      var bounds = new L.latLngBounds([]);
+      var stops;
+      if (stopIds.length > 0) {
+        stops = self.store.query('stop', {onestop_id: stopIds.join(',')});
+        stops.then(function(stops){
+          stops.forEach(function(stop){
+            bounds.extend(new L.latLng(stop.get('coordinates')));
           });
         });
+      }
+      var rsps = self.store.query('route-stop-pattern', {onestop_id: rspIds.join(',')});
+      rsps.then(function(rsps){
+        rsps.forEach(function(rsp){
+          rsp.get('coordinates').forEach(function(coord){
+            bounds.extend(new L.latLng(coord));
+          });
+        });
+      });
+
+      return Ember.RSVP.hash({
+        selectedIssue: selectedIssue,
+        issueRouteStopPatterns: rsps,
+        issueStops: stops,
+        bounds: bounds,
+        changeset: changeset,
+        users: users
       });
     });
   }
