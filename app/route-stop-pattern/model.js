38b641f8b3a8ffc2e96fff7c47d64ce75ae337bc
Alexander Smith <alex@mapzen.com>
2017-02-26 22:23:40 -0800

    Revert "single request for stopsWithDistances"
    This reverts commit 8b9d5a7043d35de6e01ebaaebe454fdfae30d681.

--- app/route-stop-pattern/model.js
+++ app/route-stop-pattern/model.js
@@ -8,6 +8,7 @@ export default EntityWithActivityModel.extend({
   trips: DS.attr(),
   stop_distances: DS.attr(),
   stop_pattern: DS.attr(),
+	stops: DS.hasMany('stop', {async: true}),
   color: DS.attr('string'),
   route_onestop_id: DS.attr('string', {readOnly: true}),
 	onestop_id: Ember.computed.alias('id'),
@@ -22,12 +23,9 @@ export default EntityWithActivityModel.extend({
 	stopsWithDistances: Ember.computed('stop_pattern', function(){
 		var self = this;
 		var args = {};
-		var stops = this.store.query('stop', { onestop_id: self.get('stop_pattern').join(',') });
-		args.promise = stops.then(function(stops) {
-			return stops.map(function(stop, index) {
-				return { stop: stop, distance: self.get('stop_distances')[index] }
-			});
-		});
+		args.promise =  Ember.RSVP.all(this.get('stop_pattern').map(function(stop_onestop_id, index){
+			return Ember.RSVP.hash({ stop: self.store.findRecord('stop', stop_onestop_id), distance: self.get('stop_distances')[index] });
+		}));
 		return Ember.ArrayProxy.extend(Ember.PromiseProxyMixin).create(args);
 	}),
 	coordinates: Ember.computed(function(){
