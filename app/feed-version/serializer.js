b1ac1c9d5a75af160693c1e8ad1e1f1d9da1496a
Ian Rees <ian@ianrees.net>
2017-02-27 16:43:57 -0800

    FeedVersion deserialize Issues

--- app/feed-version/serializer.js
+++ app/feed-version/serializer.js
@@ -1,5 +1,10 @@
 import ApplicationSerializer from '../application/serializer';
 
 export default ApplicationSerializer.extend({
-  primaryKey: 'sha1'
+  primaryKey: 'sha1',
+  attrs: {
+    issues: {
+      embedded: 'always'
+    }
+  }
 });
