cba8bd0e97bc136990d34fcc305dd76e64d9c6ce
Ian Rees <ian@ianrees.net>
2017-03-24 16:45:01 -0700

    Test

--- app/feed-version-info/model.js
+++ app/feed-version-info/model.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   feed_version: DS.belongsTo('feed-version', { async: true }),
-  data: DS.attr(),
+  // data: DS.attr(),
   created_at: DS.attr('date'),
   updated_at: DS.attr('date')
 });
