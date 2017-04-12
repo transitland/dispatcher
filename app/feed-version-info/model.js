ace58bcb4ce8ac95d60315d706b0ddb78907eb4b
Ian Rees <ian@ianrees.net>
2017-04-11 17:20:37 -0700

    Include FeedVersionInfo type

--- app/feed-version-info/model.js
+++ app/feed-version-info/model.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   feed_version: DS.belongsTo('feed-version', { async: true }),
   json: DS.attr(),
+  type: DS.attr(),
   created_at: DS.attr('date'),
   updated_at: DS.attr('date')
 });
