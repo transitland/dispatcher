68cd4ccf51fb0313bfa3a5ef653616cce7ed9115
Ian Rees <ian@ianrees.net>
2017-04-13 14:57:43 -0700

    import_level

--- app/feed-version-import/model.js
+++ app/feed-version-import/model.js
@@ -3,9 +3,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   feed: DS.belongsTo('feed', { async: true }),
   feed_version: DS.belongsTo('feed-version', { async: true }),
-
   success: DS.attr('boolean', { allowNull: true }),
   import_log: DS.attr('string'),
+  import_level: DS.attr('number'),
   exception_log: DS.attr('string'),
   validation_report: DS.attr('string'),
   created_at: DS.attr('date'),
