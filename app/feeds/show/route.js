25a71c048d28827e4f40f82abe7150eefadd3aba
Ian Rees <ian@ianrees.net>
2017-04-27 22:41:21 -0700

    Query for feedVersionInfos

--- app/feeds/show/route.js
+++ app/feeds/show/route.js
@@ -6,11 +6,24 @@ export default Ember.Route.extend({
     let feed = this.store.findRecord('feed', params['feed_id']);
     let routeGeometryIssues = this.store.query('issue', { of_feed_entities: params['feed_id'], category: 'route_geometry', per_page: 0, total: true });
     let stationHierarchyIssues = this.store.query('issue', { of_feed_entities: params['feed_id'], category: 'station_hierarchy', per_page: 0, total: true });
+    let feedVersions = this.store.query('feed-version', { feed_onestop_id: params['feed_id'] })
+    let self = this;
     return Ember.RSVP.hash({
       feed: feed,
+      feedVersions: feedVersions,
       routeGeometryIssues: routeGeometryIssues,
       stationHierarchyIssues: stationHierarchyIssues
-    });
+    }).then(function (model) {
+      let i = model.feedVersions.map(function(i){return i.id}).join(',');
+      let feedVersionInfoStatistics = self.store.query('feed-version-info', {'feed_version_sha1': i, type: 'FeedVersionInfoStatistics'} );
+      return Ember.RSVP.hash({
+        feed: model.feed,
+        feedVersions: model.feedVersions,
+        routeGeometryIssues: routeGeometryIssues,
+        stationHierarchyIssues: stationHierarchyIssues,
+        feedVersionInfoStatistics: feedVersionInfoStatistics
+      })
+    })
   },
   actions: {
     feedFetchStarted: function() {
