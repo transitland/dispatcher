a122ae32a6869db866d007162b0c6a2e8fecb53b
Alexander Smith <saalexantay@gmail.com>
2017-05-17 15:09:44 -0500

    rsp popup and removing unnecessary actions

--- app/components/route-map/route-stop-pattern/component.js
+++ app/components/route-map/route-stop-pattern/component.js
@@ -8,6 +8,10 @@ export default Ember.Component.extend({
   actions: {
     rspAdded: function(addEvent) {
       this.sendAction('rspAdded', addEvent.target._leaflet_id, this.get('onestop_id'));
+    },
+    popupOpen: function(e) {
+      let content = "<p>" + this.get('onestop_id') + "</p>"
+      e.popup.setContent(content);
     }
   }
 });
