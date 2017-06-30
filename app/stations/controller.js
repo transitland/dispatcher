16576a96e3ff86a073dddd96ef62ea03b7c7a8a4
Ian Rees <ian@ianrees.net>
2017-06-30 12:15:22 -0700

    min_platforms

--- app/stations/controller.js
+++ app/stations/controller.js
@@ -5,6 +5,16 @@ export default Ember.Controller.extend({
   lat: 37.78990,
   lng: -122.39310,
   zoom: 14,
+  isMinPlatforms: Ember.computed('min_platforms', function() {
+    console.log('isMinPlatforms', this.get('min_platforms'));
+    if (this.get('min_platforms') > 0) {
+      console.log(true);
+      return true
+    } else {
+      console.log(false);
+      return false
+    }
+  }),
   getChanges: function() {
     var entities = [];
     entities = entities.concat(this.store.peekAll('stop-station').filter(function(e) { return e.get('hasDirtyAttributes'); }));
@@ -18,6 +28,16 @@ export default Ember.Controller.extend({
     });
   },
   actions: {
+    toggleMinPlatforms: function() {
+      console.log('toggleMinPlatforms');
+      if (this.get('min_platforms') > 0) {
+        console.log('set to null');
+        this.set('min_platforms', 0);
+      } else {
+        console.log('set to 1');
+        this.set('min_platforms', 1);
+      }
+    },
     showChangeset: function() {
       var payload = {changes: this.getChanges()};
       this.model.changeset.get('change_payloads').get('firstObject').set('payload', payload);
--- app/stations/route.js
+++ app/stations/route.js
@@ -15,10 +15,15 @@ export default Ember.Route.extend({
     bbox: {
       replace: true,
       refreshModel: true
+    },
+    min_platforms: {
+      replace: true,
+      refreshModel: true
     }
   },
   model: function(params) {
     var bbox = params.bbox;
+    let min_platforms = parseInt(params.min_platforms);
     // Changeset
     let changeset = this.store.createRecord('changeset', {
       user: this.get('currentUser.user'),
@@ -26,10 +31,17 @@ export default Ember.Route.extend({
     });
     changeset.get('change_payloads').createRecord();
     // Stops
-    let stops = this.store.query('stop-station', {embed_issues: true, bbox: bbox, per_page: 1000, total: false});
+    let q = {
+      bbox: bbox,
+      per_page: 1000,
+      total: false
+    }
+    if (min_platforms > 0) {
+      q.min_platforms = min_platforms
+    }
     return Ember.RSVP.hash({
       changeset: changeset,
-      stops: stops
+      stops: this.store.query('stop-station', q)
     });
   }
 });
