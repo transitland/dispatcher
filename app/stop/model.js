e202aacae388b1775fb90590793c998ca08a3f47
Ian Rees <ian@ianrees.net>
2017-08-04 16:23:33 -0700

    Oops

--- app/stop/model.js
+++ app/stop/model.js
@@ -47,7 +47,7 @@ export default EntityWithActivityModel.extend({
       name: this.get('name'),
       timezone: this.get('timezone'),
       geometry: this.get('geometry'),
-      geometry_reversegeo: this.get('geometry_reversegeo')
+      geometryReversegeo: this.get('geometry_reversegeo')
     };
   }
 });
