d05d9b600611dc3ebad7b7ef6ee956b31430aed6
Ian Rees <ian@ianrees.net>
2017-08-07 16:38:57 -0700

    Limit per_page to 100

--- app/feeds/show/route.js
+++ app/feeds/show/route.js
@@ -6,7 +6,7 @@ export default Ember.Route.extend({
     let feed = this.store.findRecord('feed', params['feed_id']);
     let routeGeometryIssues = this.store.query('issue', { of_feed_entities: params['feed_id'], category: 'route_geometry', per_page: 0, total: true });
     let stationHierarchyIssues = this.store.query('issue', { of_feed_entities: params['feed_id'], category: 'station_hierarchy', per_page: 0, total: true });
-    let feedVersions = this.store.query('feed-version', { sort_key: 'earliest_calendar_date', sort_order: 'desc', per_page: false, feed_onestop_id: params['feed_id'] })
+    let feedVersions = this.store.query('feed-version', { per_page: 100, sort_key: 'earliest_calendar_date', sort_order: 'desc', feed_onestop_id: params['feed_id'] })
     let self = this;
     return Ember.RSVP.hash({
       feed: feed,
