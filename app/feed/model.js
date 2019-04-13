d1796732cbb63d9e2d5c829dd8e40f59761818d7
Ian Rees <ian@ianrees.net>
2019-04-12 17:42:03 -0700

    Basic support for RT Feeds

--- app/feed/model.js
+++ app/feed/model.js
@@ -15,7 +15,8 @@ export default EntityWithActivityModel.extend({
   operators_in_feed: DS.attr(),
   import_level_of_active_feed_version: DS.attr('number'),
   import_status: DS.attr('string'),
-  url: DS.attr('string'),
+  urls: DS.attr(),
+  type: DS.attr('string'),
   tags: DS.attr(),
   feed_format: DS.attr('string'),
   name: DS.attr('string'),
@@ -31,7 +32,7 @@ export default EntityWithActivityModel.extend({
   feed_versions_count: DS.attr('number'),
   created_at: DS.attr('date'),
   updated_at: DS.attr('date'),
-
+  feedFormatIsGtfsStatic: Ember.computed.equal('type', null),
   onestop_id: Ember.computed.alias('id'),
   hasIssues: Ember.computed('issues', function() {
     return this.get('issues').get('length') > 0;
@@ -56,7 +57,7 @@ export default EntityWithActivityModel.extend({
       onestopId: this.id
     };
     // TODO: move list of attributes
-    ['url', 'tags', 'feed_format', 'name', 'license_name', 'license_url', 'license_use_without_attribution', 'license_attribution_text', 'license_create_derived_product', 'license_redistribute', 'operators_in_feed'].map((attribute) => {
+    ['urls', 'tags', 'name', 'license_name', 'license_url', 'license_use_without_attribution', 'license_attribution_text', 'license_create_derived_product', 'license_redistribute', 'operators_in_feed'].map((attribute) => {
       if (Object.keys(this.changedAttributes()).includes(attribute)) {
         change[attribute] = this.get(attribute);
       }
